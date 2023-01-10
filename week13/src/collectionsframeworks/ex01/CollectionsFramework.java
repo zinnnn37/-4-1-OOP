43e4792855d1e2a64e42ae675dad32a0eaf7b112
zinnnn37 <zinnnn37@gmail.com>
2023-01-10 13:11:25 +0900

    [week13, 14] init

---
+++ week13/src/collectionsframeworks/ex01/CollectionsFramework.java
@@ -0,0 +1,10 @@
+package collectionsframeworks.ex01;
+
+public class CollectionsFramework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ week14/src/iostream/ex01/IOStreamStart.java
@@ -0,0 +1,10 @@
+package iostream.ex01;
+
+public class IOStreamStart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
